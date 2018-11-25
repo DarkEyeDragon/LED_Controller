4d28cea24a755804fd46563823ef0a82c24bc9e2
DarkEyeDragon <joshua.tack@student.vives.be>
2018-11-25 11:48:59 +0100

    Added debug window

---
+++ LED_Controller/Debug/ConsoleWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LED_Controller.Debug
+{
+    public partial class ConsoleWindow : Window
+    {
+        private SerialPort _serialPort;
+        Task task;
+        public ConsoleWindow(ref SerialPort serialPort)
+        {
+            InitializeComponent();
+            if (serialPort != null)
+            {
+                _serialPort = serialPort;
+                _serialPort.DataReceived += DataReceived;
+            }
+        }
+
+        private void DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (_serialPort != null)
+                AppendText(((SerialPort) sender).ReadLine());
+        }
+
+        public void AppendText(string text)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                if (Output.Inlines.Count > 100)
+                {
+                    Output.Inlines.Remove(Output.Inlines.FirstInline);
+                }
+
+                Output.Inlines.Add(text);
+                ScrollViewer.ScrollToBottom();
+            });
+        }
+    }
+}
